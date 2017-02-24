823b0a1f9345036c0d306d5843a812d172e731c9
Matt Ferraro <mferraro89@gmail.com>
2017-02-24 13:04:07 -0800

    starting the cl stuff

--- design.js
+++ design.js
@@ -1,18 +1,19 @@
 function redraw() {
-	let canvas = $("#mainCanvas")[0];
-	let ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#FFFFFF";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
-
-	ctx.fillStyle = "#000000";
-
-	ctx.fillRect(0,canvas.height / 2, canvas.width, 1);
-
 	let nPoints = 100;
 	let chord = 20;
 	let m = $("#maxCamber").val() / 100; // 100m is the first digit of a NACA 4 digit
 	let p = $("#maxCamberLoc").val() / 10;  // 10p is the second digit of a NACA 4 digit
 	let t = $("#thickness").val() / 100; // 100t is the last 2 digits of a NACA 4 digit
+
+	if (m*100 % 1 > .49 && m*100 % 1 < .51) {
+		var mFormatted = Math.floor(m*100) + ".5";
+	}
+	else if (m*100 % 1 > .24 && m*100 % 1 < .26) {
+		var mFormatted = Math.floor(m*100) + ".25";
+	}
+	else {
+		var mFormatted = Math.floor(m*100) + "";
+	}
 
 	if (Math.round(t * 100) < 10) {
 		var tFormatted = "0" + Math.round(t * 100);
@@ -20,7 +21,7 @@ function redraw() {
 	else {
 		var tFormatted = Math.round(t * 100);
 	}
-	$("#shape").html(m * 100 + "" + p * 10 + "" + tFormatted);
+	$("#shape").html(mFormatted + "" + p * 10 + "" + tFormatted);
 
 	let topxs = [];
 	let bottomxs = [];
@@ -28,24 +29,32 @@ function redraw() {
 	let topys = [];
 	let bottomys = [];
 	let camberys = [];
+	let camberSlopes = [];
+	let thetas = [];
 
 	// Generate the points
 	for (let i = 0; i < nPoints; i++) {
-		let x = 0.5 * (1 - Math.cos(i / nPoints * 3.14159)) * chord;
+		let bigTheta = i / nPoints * 3.14159;
+		let x = 0.5 * (1 - Math.cos(bigTheta)) * chord;
 		let xOverC = x / chord;
 		camberxs.push(x);
+		thetas.push(bigTheta);
 
 		let cambery;
+		let camberSlope;
 		let theta;
 		if (xOverC <= p) {
 			cambery = m / (p * p) * (2 * p * xOverC - xOverC * xOverC) * chord;
-			theta = Math.atan(2 * m / (p * p) * (p - xOverC));
+			camberSlope = 2 * m / (p * p) * (p - xOverC);
+			theta = Math.atan(camberSlope);
 		}
 		else {
 			cambery = m / ((1 - p) * (1 - p)) * ((1 - 2 * p) + 2 * p * xOverC - xOverC * xOverC) * chord;
-			theta = Math.atan(2 * m / ((1 - p) * (1 - p)) * (p - xOverC));
+			camberSlope = 2 * m / ((1 - p) * (1 - p)) * (p - xOverC);
+			theta = Math.atan(camberSlope);
 		}
 		camberys.push(cambery);
+		camberSlopes.push(camberSlope);
 
 		let yt = chord * 5 * t * (.2969 * Math.sqrt(xOverC) - .1260 * xOverC - 0.3516 * Math.pow(xOverC, 2) + 0.2843 * Math.pow(xOverC, 3) - 0.1015 * Math.pow(xOverC, 4));
 
@@ -58,6 +67,39 @@ function redraw() {
 		bottomys.push(bottomY);
 	}
 
+	drawProfile(chord, topxs, topys, bottomxs, bottomys, camberxs, camberys);
+
+	drawCl(thetas, camberxs, camberSlopes);
+}
+
+function drawCl(thetas, camberxs, camberSlopes) {
+	// we have a bunch of big thetas and the associated camberSlopes.
+	// this is our version of f(theta)
+	for (let alpha = -4; alpha<10; alpha += 0.5) {
+
+	}
+
+	let canvas = $("#clCanvas")[0];
+	let ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,canvas.height / 2, canvas.width, 1);
+	ctx.fillRect(canvas.width/2, 0, 1, canvas.height);
+}
+
+function drawProfile(chord, topxs, topys, bottomxs, bottomys, camberxs, camberys) {
+	let nPoints = topxs.length;
+
+	let canvas = $("#mainCanvas")[0];
+	let ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,canvas.height / 2, canvas.width, 1);
 
 	// Draw the top curve
 	ctx.strokeStyle="#0000FF";
