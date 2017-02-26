1aaa141f3c347091eda130a9794c73ce56ef9eb6
Matt Ferraro <mferraro89@gmail.com>
2017-02-26 15:25:18 -0800

    cl vs alpha now draws

--- design.js
+++ design.js
@@ -75,19 +75,79 @@ function redraw() {
 function drawCl(thetas, camberxs, camberSlopes) {
 	// we have a bunch of big thetas and the associated camberSlopes.
 	// this is our version of f(theta)
-	for (let alpha = -4; alpha<10; alpha += 0.5) {
+	let alphas = [];
+	let cls = [];
+	for (let alpha = -5; alpha<=10; alpha += 0.5) {
+		alphaRadians = alpha * 3.14159 / 180;
+		let angleDelta = 3.14159 / (camberSlopes.length - 1);
+		let A0 = 0;
+		let A1 = 0;
+		let fourierCoefficients = [];
 
+		for (let fci = 0; fci < 10; fci++) {
+			let fc = 0;
+			for (let i = 0; i < thetas.length; i++) {
+				let slope = -camberSlopes[i];
+				let f_theta = alphaRadians - slope;
+				fc += f_theta * Math.cos(thetas[i] * fci);
+				// A0 += f_theta;
+				// A1 += f_theta * Math.cos(thetas[i]);
+			}
+			fc *= angleDelta / 3.14159;
+			fourierCoefficients.push(fc);
+		}
+		// A0 *= angleDelta / 3.14159;
+		// A1 *= angleDelta / 3.14159;
+		// let cl = 3.14159 * (2 * A0 + A1);
+		let cl = 3.14159 * (2 * fourierCoefficients[0] + fourierCoefficients[1]);
+		cls.push(cl);
+		alphas.push(alphaRadians);
 	}
 
 	let canvas = $("#clCanvas")[0];
 	let ctx = canvas.getContext("2d");
 
+	// Clear the background
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 
-	ctx.fillStyle = "#000000";
+	// Draw the axes
+	ctx.fillStyle = "#555";
 	ctx.fillRect(0,canvas.height / 2, canvas.width, 1);
 	ctx.fillRect(canvas.width/2, 0, 1, canvas.height);
+
+	// Draw the vertical hashes
+	ctx.strokeStyle = "#AAAAAA";
+	for (let i = -10; i <= 10; i ++) {
+		ctx.beginPath();
+		ctx.moveTo(canvas.width / 2 + i * 20, 0);
+		ctx.lineTo(canvas.width / 2 + i * 20, canvas.height);
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.moveTo(0, canvas.height / 2 + i * 10);
+		ctx.lineTo(canvas.width, canvas.height / 2 + i * 10);
+		ctx.stroke();
+	}
+
+	// Draw the cl vs alpha graph
+	ctx.strokeStyle="#0000FF";
+	ctx.beginPath();
+	for (let i = 0; i < alphas.length; i++) {
+		let alpha = alphas[i];
+		let cl = cls[i];
+
+		let graphicalX = alpha * 180 / 3.14159 * 20 + canvas.width / 2;
+		let graphicalY = canvas.height / 2 - cl * 50;
+		if (i === 0) {
+			ctx.moveTo(graphicalX, graphicalY);
+		}
+		else {
+			ctx.lineTo(graphicalX, graphicalY);
+		}
+	}
+	ctx.stroke();
+
 }
 
 function drawProfile(chord, topxs, topys, bottomxs, bottomys, camberxs, camberys) {
